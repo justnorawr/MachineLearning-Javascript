6004934b81529b1b9cee6bb29931ce01402184ce
Nicholas Curtis <nich.curtis@gmail.com>
2014-03-28 17:21:04 -0700

    Merged single and multi var regression class into one
    updated multi to accept a variable number of features

--- LinearRegression.js
+++ LinearRegression.js
@@ -1,71 +1,83 @@
-
 function LinearRegression (theta, theta1, alpha, iterations) {
-    this.initial_theta = [theta, theta1];
-    this.alpha = alpha;
-    this.iterations = iterations;
+	this.initial_theta = [theta, theta1];
+	this.alpha = alpha;
+	this.iterations = iterations;
 
-    this.train = function (x, y) {
-        console.log('Starting Theta', this.initial_theta);
-        this.theta = this.initial_theta;
-        this.gradientDescent(x, y);
-        console.log('Final Theta', this.theta);
-        return true;
-    };
+	this.train = function (X, Y) {
+		this.theta = this.initial_theta;
+		this.costHistory = [];
+		this.gradientDescent(X, Y);
+		return true;
+	};
 
-    this.predict = function (x) {
-        return this.theta[0] + (this.theta[1] * x);
-    }
+	this.predict = function (x) {
+		var prediction = 0;
+		for (var j=0; j<x.length; j++)
+		{
+			prediction += this.theta[j] * x[j];
+		}
+		return prediction;
+	}
 
-    this.gradientDescent = function (X, Y) {
+	this.computeCost = function (theta, x, y)
+	{
+		this.theta = theta;
+		this.buildTheta(x.length);
 
-        for (var i=0; i<this.iterations; i++) {
+		var h = 0;
 
-            var errors = 0;
-            var errors2 = 0;
-            for (var j=0; j<Y.length; j++)
-            {
-                var x = X[j];
-                var y = Y[j];
+		for (var k=0; k<x.length; k++)
+		{
+			h += this.theta[k] * x[k];
+		}
 
-                var h = this.theta[0] + ( this.theta[1] * x );
-                errors = errors + (h-y);
-                errors2 = errors + (h-y)*x;
-            }
+		return 1/x.length + (h-y);
+	}
 
-            var delta = (1/Y.length) * errors;
-            var temp1 = this.theta[0] - (this.alpha * delta);
+	this.buildTheta = function (features) {
+		for (var i=2; i<features; i++) {
+			this.initial_theta[i] = this.initial_theta[1];
+		}
+	}
 
-            var delta2 = (1/Y.length) * errors2;
-            var temp2 = this.theta[1] - (this.alpha * delta2);
+	this.gradientDescent = function (X, Y) {
 
-            this.theta[0] = temp1;
-            this.theta[1] = temp2;
+		this.buildTheta(X.length);
 
-            // console.log(this.theta);
-            // console.log(this.theta[0] + (this.theta[1] * 15));
-        }
-    };
+		// compute one step of gradient descent for each iteration
+		for (var i=0; i<this.iterations; i++) {
+
+			var temp = [];
+
+			// compute cost of each feature
+			// J(j1, j2, j3) = 1/2m( (Theta0 + Theta1*X1 + Theta2*X2) - Y )^2
+			for (var j=0; j<X.length; j++)
+			{
+				var x = X[j];
+				var y = Y[j];
+				var h = 0;
+
+				for (var k=0; k<x.length; k++)
+				{
+					h += this.theta[k] * x[k];
+				}
+
+				var cost = [];
+				for (var k=0; k<x.length; k++) {
+					cost[k] = (h-y) * x[k];
+					var delta = (1/Y.length) * cost[k];
+					temp[k] = this.theta[k] - (this.alpha * delta);
+				}
+
+				this.costHistory[j] = {
+					cost: cost[1] * x[1],
+					theta: temp,
+					hypothesis: h
+				};
+			}
+
+			// update theta values at this step
+			this.theta = temp;
+		}
+	};
 };
-
-
-features = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-targets = [ 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
-theta = .1, alpha = .1, iterations = 1000;
-
-LRModel = new LinearRegression(0, theta, alpha, iterations);
-LRModel.train(features, targets);
-
-prediction = LRModel.predict(44);
-console.log( 'For Value: ', 44, ' Predicted: ', prediction );
-prediction = LRModel.predict(34);
-console.log( 'For Value: ', 34, ' Predicted: ', prediction );
-prediction = LRModel.predict(24);
-console.log( 'For Value: ', 24, ' Predicted: ', prediction );
-
-for (var t=1; t<10; t++)
-{
-    test = t * Math.random();
-    prediction = LRModel.predict(test);
-
-    console.log( 'For Value: ', test, ' Predicted: ', prediction );
-}
